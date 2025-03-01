ea6dfd1a91dd9da4996441f1b918e636dbfcfc38
alan llamas <alanllamasg@gmail.com>
2025-02-28 19:12:02 -0600

    chore: removed dummy

--- src/api/utils/dummy/data.ts
+++
@@ -1,5 +0,0 @@
-export const HOLOGRAM_REPOSITION_PROCEDURE_INFO = {
-  title: "Reposición de Holograma",
-  description: "Es un documento con el cual se identifica el estudiante como parte de la comunidad de la universidad.",
-  price: 110
-};
