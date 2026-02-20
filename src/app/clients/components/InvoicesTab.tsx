707c7d1e4bedea97ffd90a81cee67e760a5a1165
alanllamas <alanllamasg@gmail.com>
2026-02-19 19:15:10 -0600

    feat: added invoices tab base

---
+++ src/app/clients/components/InvoicesTab.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from "react";
+import { TabPanel } from '@headlessui/react'
+import TicketList from "@/components/ticketList";
+
+const InvoicesTab: React.FC<any> = ({tickets, clientId}) => {
+  return (
+    <TabPanel className="px-4 q">
+      <TicketList ticketData={tickets} itemsPerPage={10} clientId={clientId}/>
+    </TabPanel>
+  )
+}
+ export default InvoicesTab
